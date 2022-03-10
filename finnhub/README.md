e6fb56cb3a2bba8b967cacb117392e8d1ac264e3
Lauri Kananen <lauri.kananen@gmail.com>
2022-03-10 17:53:30 +0200

    Finnhub websocket example added.

--- finnhub/README.md
+++ finnhub/README.md
@@ -5,3 +5,5 @@
 2. Copy [example secrets file](./secrets_example.py) as [secrets file](./secrets.py)
 3. Add personal Finnhub API key to [secrets file](secrets.py).
 
+## Example queries
+- https://github.com/Finnhub-Stock-API/finnhub-python
--- finnhub/test_api.py
+++ finnhub/test_api.py
@@ -1,9 +1,59 @@
-import finnhub
 from secrets import API_KEY
+import finnhub
+import time
+import websocket
 
-# Setup client
-finnhub_client = finnhub.Client(api_key=API_KEY)
 
-# Stock candles
-res = finnhub_client.stock_candles('AAPL', 'D', 1590988249, 1591852249)
-print(res)
+##############################################################################
+# Stock market data
+
+def test_stock():
+    # Setup client
+    finnhub_client = finnhub.Client(api_key=API_KEY)
+    
+    # Some data sources
+    #print(finnhub_client.covid19())
+    #print(finnhub_client.fda_calendar())
+
+    # Stock candles
+    time_delta_seconds = 60*5
+    res = finnhub_client.stock_candles('AAPL', 'D', int(time.time() - time_delta_seconds), int(time.time()))
+    print(res)
+
+
+##############################################################################
+# Websocket
+
+def on_message(ws, message):
+    print(message)
+
+
+def on_error(ws, error):
+    print(error)
+
+
+def on_close(ws):
+    print("### closed ###")
+
+
+def on_open(ws):
+    ws.send('{"type":"subscribe","symbol":"AAPL"}')
+    ws.send('{"type":"subscribe","symbol":"AMZN"}')
+    ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+    ws.send('{"type":"subscribe","symbol":"IC MARKETS:1"}')
+
+
+def test_websocket():
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://ws.finnhub.io?token=c8l0rlaad3icvur3l0lg",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+##############################################################################
+
+
+if __name__ == "__main__":
+    test_websocket()
