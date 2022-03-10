acfc2151dc4fcc258899035b1947a90a0f53674e
Lauri Kananen <lauri.kananen@gmail.com>
2022-03-10 18:10:19 +0200

    Finance symbol fetching function added.

--- finnhub/README.md
+++ finnhub/README.md
@@ -2,8 +2,10 @@
 
 ## Setup
 1. Create Finnhub account: [link](https://finnhub.io/dashboard)
-2. Copy [example secrets file](./secrets_example.py) as [secrets file](./secrets.py)
+2. Rename [example secrets file](./secrets_example.py) as [secrets file](./secrets.py)
 3. Add personal Finnhub API key to [secrets file](secrets.py).
+4. Install [requirements](requirements.txt).
+5. Run [the test file](test_api.py).
 
 ## Example queries
 - https://github.com/Finnhub-Stock-API/finnhub-python
--- finnhub/test_api.py
+++ finnhub/test_api.py
@@ -16,9 +16,14 @@ def test_stock():
     #print(finnhub_client.fda_calendar())
 
     # Stock candles
-    time_delta_seconds = 60*5
+    time_delta_seconds = 60 * 5
     res = finnhub_client.stock_candles('AAPL', 'D', int(time.time() - time_delta_seconds), int(time.time()))
     print(res)
+    
+
+def get_symbols(exhange="BINANCE"):
+    finnhub_client = finnhub.Client(api_key=API_KEY)
+    print(finnhub_client.crypto_symbols(exhange))
 
 
 ##############################################################################
@@ -56,4 +61,8 @@ def test_websocket():
 
 
 if __name__ == "__main__":
-    test_websocket()
+    #test_stock()
+    
+    get_symbols()
+    
+    #test_websocket()
