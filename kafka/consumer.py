1353468ededc855b2d72a9e0953ddbe74d8d8e63
Lauri Kananen <lauri.kananen@gmail.com>
2022-03-17 19:01:15 +0200

    Consumer parsing added to the topic data.

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,17 +1,28 @@
 from kafka import KafkaConsumer
-from json import loads
-from time import sleep
-consumer = KafkaConsumer(
-    'topic_test',
-    bootstrap_servers=['localhost:9092'],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='my-group-id',
-    value_deserializer=lambda x: loads(x.decode('utf-8'))
-)
-for event in consumer:
-    event_data = event.value
-    # Do whatever you want
-    print(event_data)
-    sleep(0.5)
+import json
+import time
+
+
+def run_consumer():
+    consumer = KafkaConsumer(
+        'topic_test',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='my-group-id',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+    for event in consumer:
+        event_data = event.value
+        # Do whatever you want
+        print([val["p"] for val in json.loads(event_data)["data"]])
+        time.sleep(0.5)
+        
+        
+def main():
+    run_consumer()
+    
+
+if __name__ == "__main__":
+    main()
     
