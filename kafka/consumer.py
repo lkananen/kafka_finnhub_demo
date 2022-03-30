943efe09df1e5d7411a5e98101fef54e446b5957
Lauri Kananen <lauri.kananen@gmail.com>
2022-03-30 10:56:56 +0300

    Buffer and plotting added to consumer.

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,9 +1,46 @@
 from kafka import KafkaConsumer
 import json
 import time
+import datetime
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+##############################################################################
+# Initial configurations
+
+plt.style.use("ggplot")
+
+
+##############################################################################
+# Buffers
+
+BUFFER_SIZE = 20
+VALUE_BUFFER = collections.deque()
+
+
+def create_value_buffer(n_stored: int = BUFFER_SIZE):
+    global VALUE_BUFFER
+    VALUE_BUFFER = collections.deque(maxlen=n_stored)
+    
+    # Initializes empty buffer
+    for i in range(BUFFER_SIZE):
+        VALUE_BUFFER.append(i)
+        
+    print("Buffer initialized!")
+
+
+def create_buffers():
+    create_value_buffer()
+    
+##############################################################################
+# Kafka consumer
 
 
 def run_consumer():
+    global VALUE_BUFFER
+    
     consumer = KafkaConsumer(
         'topic_test',
         bootstrap_servers=['localhost:9092'],
@@ -12,17 +49,76 @@ def run_consumer():
         group_id='my-group-id',
         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
+    
+    print("Message read - " + str(datetime.datetime.now()))
+
+    # Plotting line initialization
+    line = []
+
     for event in consumer:
         event_data = event.value
-        # Do whatever you want
-        print([val["p"] for val in json.loads(event_data)["data"]])
-        time.sleep(0.5)
         
+        # Fills the new values to the buffer
+        values = [val["p"] for val in json.loads(event_data)["data"]]
+        for val in values:
+            VALUE_BUFFER.append(val)
+            
+        newest_values = list(VALUE_BUFFER)
+        print(newest_values)
+        line = plot_graph(newest_values, line=line)
+        
+        time.sleep(0.5)
+
+##############################################################################
+# Plotting
+
+
+def plot_graph(y,
+               x=[],
+               line=[]):
+    
+    # If no data is provided, plotting cannot be done
+    if not y:
+        return []
+    
+    # Generates x marks if not provided
+    if x == []:
+        x = [i for i in range(len(y))]
+    
+    if line == []:
+        plt.ion()       # interactive mode
+        fig = plt.figure(figsize=(6, 3))
+        ax = fig.add_subplot(111)
+        line, = ax.plot(x, y, '-o', alpha=0.8)
+        
+        plt.ylabel("Y")
+        plt.title("Title")
+        plt.show()
+
+    # Updates the y-values and the axis bounds
+    line.set_ydata(y)
+    if np.min(y) <= line.axes.get_ylim()[0] or np.max(y) >= line.axes.get_ylim()[1]:
+        plt.ylim([
+            np.min(y) - np.std(y),
+            np.max(y) + np.std(y)
+        ])
+    if np.min(x) <= line.axes.get_xlim()[0] or np.max(x) >= line.axes.get_xlim()[1]:
+        plt.xlim([
+            np.min(x) - np.std(x),
+            np.max(x) + np.std(x)
+        ])
+    
+    plt.pause(0.1)
+    
+    return line
+
+##############################################################################
+
         
 def main():
+    create_buffers()
     run_consumer()
     
 
 if __name__ == "__main__":
     main()
-    
--- kafka/producer.py
+++ kafka/producer.py
@@ -3,33 +3,39 @@ from kafka import KafkaProducer
 import websocket
 import finnhub
 import time
+import datetime
+import secrets
 
 
-def on_message(ws, message):
+def on_message(ws: websocket.WebSocketApp, message: str):
+    TOPIC_NAME = "topic_test"
+    
     producer = KafkaProducer(
         bootstrap_servers=['localhost:9092'],
         value_serializer=lambda x: dumps(x).encode('utf-8')
     )
     
-    producer.send('topic_test', value=message)
+    producer.send(TOPIC_NAME, value=message)
+    print("Message sent - " + str(datetime.datetime.now()) + " - " + str(TOPIC_NAME))
     
 
-def on_error(ws, error):
+def on_error(ws: websocket.WebSocketApp, error: str):
     print(error)
 
 
-def on_close(ws):
+def on_close(ws: websocket.WebSocketApp):
     print("### closed ###")
 
 
-def on_open(ws):
-    ws.send('{"type":"subscribe","symbol":"AMZN"}')
+def on_open(ws: websocket.WebSocketApp):
+    ws.send('{"type":"subscribe","symbol":"BINANCE:BTCUSDT"}')
+    #ws.send('{"type":"subscribe","symbol":"AMZN"}')
 
 
 def run_producer():
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(
-        "wss://ws.finnhub.io?token=c8l0rlaad3icvur3l0lg",
+        "wss://ws.finnhub.io?token=" + secrets.API_KEY,
         on_message=on_message,
         on_error=on_error,
         on_close=on_close
